4672ae67197953b6ce608483add0bd896d66eaa9
taraknight-tk <tara.knight@outlook.com>
2026-02-01 07:25:20 -0700

    Initial Commit

---
+++ Theme Showcase/Theme_ShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Theme_ShowcaseApp.swift
+//  Theme Showcase
+//
+//  Created by Tara Knight on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Theme_ShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
